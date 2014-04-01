5d7f2285b4b85efd772a88832e9013b709ea18b3
Luchia Tseng <luchia.tseng@gmail.com>
2014-03-31 22:04:39 -0700

    test folder added

---
+++ test/com/google/gwt/ParkIt/ParkingDataParserTest.java
@@ -0,0 +1,14 @@
+package com.google.gwt.ParkIt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParkingDataParserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
